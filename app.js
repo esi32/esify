0798ea332a01ca02de7febc7ac2ddc848acfbd51
Ehsan Hejazizadeh <ehsan@Ehsans-MacBook-Pro.local>
2020-04-08 13:21:29 +0430

    Remove consoles

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use('/' , (req, res)=>{
 	res.send('<h1>server is runnig</h1>')
 });
 
-app.listen(port);
+app.listen(3000);
