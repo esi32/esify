47bedb3cf86c599e7b5073be1341416b6e9ddbc4
Ehsan Hejazizadeh <ehsan@Ehsans-MacBook-Pro.local>
2020-04-08 14:31:30 +0430

    public

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use('/' , (req, res)=>{
 	res.send('<h1>server is runnig</h1>')
 });
 
-app.listen(3000);
+app.listen(port);
